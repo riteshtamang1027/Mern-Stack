fd6e92639ddb70950f3943e889fb8f70c4bafcba
Ritesh Tamang <riteshtmglama001@gmail.com>
2025-02-22 22:00:39 +0545

    add CRUDE

--- secondapp/backend/server.js
+++ secondapp/backend/server.js
@@ -13,54 +13,100 @@ try {
 }
 
 // Make Schema Table
-
 const StudentSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  roll_no: { type: Number, required: true },
+  roll_no: { type: Number, required: true, unique: true },
+  student_name: { type: String, required: true },
   registation_no: { type: Number, required: true },
   parent_name: { type: String, required: true },
-  phone_no: { type: Number, required: true },
+  phone_number: { type: Number, required: true },
 });
 
-const Student = mongoose.model("Student", StudentSchema);
+// Make Table
+const Students = mongoose.model("Students", StudentSchema);
+// CRUDE of Students
 
+// 1. Create Section
 
-// Create a Table of student
-app.post("/Student", async (req, res) => {
+app.post("/Students", async (req, res) => {
   try {
-   
-   
-    // const ExistStudent = await newStudent.findOne({name:req.body.name})
-    // if(ExistStudent){
-    //   return res.status(409).json({
-    //     message:"This name is already exist, Please Try another name",
-    //   })
-    // }
-
-    
-    const newStudent= await new Student(req.body).save();
-    // console.log(newStudent)
-    return res.status(201).json(
-      {
-        message:"Data create successfully !",
-        data:newStudent
+    const ExistName = await Students.findOne({
+      student_name: req.body.student_name,
+    });
+    if (ExistName) {
+      return res.status(409).json({
+        message: "This name already exist, please enter another name.",
       });
-
- 
-
+    }
+    const newStudent = await new Students(req.body).save();
+    return res.status(201).json({
+      message: "Student data create Successfully",
+      data: newStudent,
+    });
+    console.log(newStudent);
   } catch (error) {
-    return res.status(500).json(
-      {
-        message:"Internet Server error",error
-      }
-    )
+    return res.status(500).json({
+      message: "Internet server error",
+    });
   }
+});
 
+// 2.1. Get all Students data
+app.get("/Students", async (req, res) => {
+  try {
+    const getallStudent = await Students.find();
+    return res.status(200).json({
+      message: "All students data fetch successfully.",
+      data: getallStudent,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      message: "Internet server error.",
+    });
+  }
+});
+
+// 2.2. Get single student data
+
+app.get("/Students/:id", async (req, res) => {
+  try {
+    const singlestudent = await Students.findById(req.params.id);
+    return res.status(200).json({
+      message: "Single Student data fetch successfully.",
+      data: singlestudent,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      message: "Internet server error.",
+    });
+  }
+});
+
+// 3. Delete Student data
+app.delete("/Students/:id", async (req, res) => {
+  try {
+    const Checkdata = await Students.findByIdAndDelete(req.params.id);
+    if (!Checkdata) {
+      return res.status(404).json({
+        message: "Student data is not available.",
+      });
+    }
+
+    const deletedata = await Students.findByIdAndDelete(req.params.id);
+    return res.status(201).json({
+      message: "This Student data delete successfully.",
+      data: deletedata,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      message: "Internet server error",
+    });
+  }
+});
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
 });
 
 app.listen(4001, () => {
-  console.log("Server is running http://localhost:4001");
-});
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+  console.log("Server is running on http://localhost:4001");
 });
