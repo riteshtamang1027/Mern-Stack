cdbb2b0bd6b4b019bd2599267281b35f769c0b17
Ritesh Tamang <riteshtmglama001@gmail.com>
2025-12-15 21:05:41 +0545

    add basick react application

--- reactapp/basicapp/src/App.jsx
+++ reactapp/basicapp/src/App.jsx
@@ -1,35 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {useState } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App(props) {
+  const [count, setCount] = useState(0);
+  const sumf =()=>{
+    let num1 = 23
+    let num2= 21
+   let sum = 0
+   sum = num1 + num2;
+   alert("the sum of the given number is "+sum)
+  }
+ 
+ const myfun= ()=>{
+  let x = 12;
+  let y="Banana";
+  if(x<10){
+    y="Apple"
+  }
 
+  alert("This is a "+y)
+
+  }
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className='text-2xl'>
+     <p> Hello World!</p>
+      <button onClick={()=>setCount(count+1)}  className="border border-gray-300 bg-gray-200 shadow-2xl rounded-md px-2 py-1">Click Here {count}</button>
+
+        <button onClick={sumf} className="border border-gray-300 bg-gray-200 shadow-2xl rounded-md px-2 py-1 cursor-pointer">Click Here   </button>
+
+        <button onClick={myfun} className="border border-gray-300 bg-gray-200 shadow-2xl rounded-md px-2 py-1 cursor-pointer">Click Here   </button>
+
+    </div>
   )
 }
 
-export default App
