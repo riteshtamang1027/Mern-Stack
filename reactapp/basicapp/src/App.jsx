81ea9889207c76ecb6c1146ae10dc9292ae94318
Ritesh Tamang <riteshtmglama001@gmail.com>
2025-12-25 15:46:14 +0545

    add skill list

--- reactapp/basicapp/src/App.jsx
+++ reactapp/basicapp/src/App.jsx
@@ -1,35 +1,55 @@
-import {useState } from "react";
+import React, { useState } from 'react'
 
-export default function App(props) {
-  const [count, setCount] = useState(0);
-  const sumf =()=>{
-    let num1 = 23
-    let num2= 21
-   let sum = 0
-   sum = num1 + num2;
-   alert("the sum of the given number is "+sum)
-  }
- 
- const myfun= ()=>{
-  let x = 12;
-  let y="Banana";
-  if(x<10){
-    y="Apple"
-  }
-
-  alert("This is a "+y)
-
-  }
+export default function App() {
+  const [isAdding, setisAdding] = useState(false);
+  const [skills, setSkills] = useState(["Frontend developer"]);
+  const [newSkill, setNewSkill] = useState("");
   return (
-    <div className='text-2xl'>
-     <p> Hello World!</p>
-      <button onClick={()=>setCount(count+1)}  className="border border-gray-300 bg-gray-200 shadow-2xl rounded-md px-2 py-1">Click Here {count}</button>
+    <div className='w-full
+    flex items-center justify-center'>
+      <div className='  border w-max mt-12 p-4 rounded-xl bg-gray-200 shadow-2xl space-y-4'>
+        {/* skills list section */}
+        <div>
+          <p className='text-xl font-bold'>Skills List :</p>
+          <ol>
+            {
+              skills.map((skill, index)=>(
+                <li key={index} className='text-base font-semibold opacity-80 ml-2'>{skill}</li>
+              ))
+            }
+          </ol>
+        </div>
+        {
+          isAdding ? <div className='space-y-2'>
+            <input type="text" value={newSkill} onChange={(e)=>setNewSkill(e.target.value)} className='outline-none border border-gray-300 rounded-xl px-2 py-1' />
+            <div className='flex items-center justify-end gap-4'>
+              <button  onClick={()=>{
+                  if(newSkill.trim()){
+                    setSkills([...skills, newSkill]);
+                    setNewSkill("");
+                    setisAdding(!isAdding)
+                  }
 
-        <button onClick={sumf} className="border border-gray-300 bg-gray-200 shadow-2xl rounded-md px-2 py-1 cursor-pointer">Click Here   </button>
+              }}
+               className="px-2 py-1 bg-green-500 rounded-lg text-white cursor-pointer">Save</button>
+              <button onClick={()=>{setisAdding(!isAdding);
+                setNewSkill("")
+              }} className="px-2 py-1 bg-red-500 rounded-lg text-white cursor-pointer">Cancel</button>
 
-        <button onClick={myfun} className="border border-gray-300 bg-gray-200 shadow-2xl rounded-md px-2 py-1 cursor-pointer">Click Here   </button>
+            </div>
+          </div> :
+            /* btn section */
+            <div>
+              <button onClick={() => setisAdding(!isAdding)} className='px-4 py-1 rounded-md bg-sky-500 text-white cursor-pointer'>
+                Add
+              </button>
+            </div>
+        }
 
+
+
+
+      </div>
     </div>
   )
 }
-
