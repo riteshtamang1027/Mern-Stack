7297be418f41d5f9438dc0f74e147c04e67362e9
Ritesh Tamang <riteshtmglama001@gmail.com>
2026-01-09 20:47:09 +0545

    add navlist

--- reactapp/basicapp/src/components/navbar_section.jsx
+++ reactapp/basicapp/src/components/navbar_section.jsx
@@ -5,21 +5,51 @@ export default function Navbar_section() {
     <>
       <nav className='flex  items-center justify-between'>
         <p>Logo</p>
-        <ol className='flex gap-4 '>
-            <li>Home</li>
-            <li>About</li>
-            <li>Contact</li>
-            <li>FAQ's</li>
+        <ol className='flex gap-8 '>
+          {
+            navItem.map((item,  index)=>(
+              <li key={index}>{item.itemName}</li>
+
+            ))
+          }
         </ol>
 
-        <div>
-
+        <div className='flex  items-center  gap-4 text-white'>
         <input className='border border-gray-200 rounded-lg outline-none px-2 py-1 text-sm' type="text" name="" id="" placeholder='Search bar' />
+
+        <LogIn size={20} className='cursor-pointer' />
         </div>
 
-        <LogIn size={20} />
 
       </nav>
     </>
   )
 }
+
+
+const navItem = [
+  {
+    itemName: "Home",
+    url:""
+  },
+    {
+    itemName: "About",
+    url:""
+  }, 
+   {
+    itemName: "Contact",
+    url:""
+  }, 
+   {
+    itemName: "Category",
+    url:""
+  },  
+  {
+    itemName: "Dashboard",
+    url:""
+  }, 
+   {
+    itemName: "Blogs",
+    url:""
+  },
+]
