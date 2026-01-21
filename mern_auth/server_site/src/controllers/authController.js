93e448f60dddd3d41f5fcf43f232ab476ab8c775
Ritesh Tamang <riteshtmglama001@gmail.com>
2026-01-21 21:52:20 +0545

    add cookie

--- mern_auth/server_site/src/controllers/authController.js
+++ mern_auth/server_site/src/controllers/authController.js
@@ -2,6 +2,7 @@
 import userModel from "../models/users_model.js"
 import jwt from 'jsonwebtoken'
 import bcrypt from 'bcrypt'
+import cookie from 'cookie'
 
 
 export const register = async (req, res) => {
@@ -21,7 +22,13 @@ export const register = async (req, res) => {
         const newUser = await new userModel({username, email, password:hashedPassword}).save()
 
         const token =  jwt.sign({id: username._id},process.env.JWT_SECRET,{expiresIn:"1d"})
-
+        res.cookie('token',token,{
+            httpOnly:true,
+            secure:process.env.NODE_ENV==='production',
+            sameSite:process.env.NODE_ENV == 'production'?'none':'strict',
+            maxAge:1*60*60*1000
+        })
+    
 
     } catch (error) {
         return res.status(500).json({ success: false, message: "Internal server error" })
