dea1e11e01a02080e694091c8cfe0e504c38f962
Ritesh Tamang <riteshtmglama001@gmail.com>
2026-09-01 12:59:16 +0545

    add percentage calculation function

--- calculator/main.js
+++ calculator/main.js
@@ -7,12 +7,12 @@ const buttons = document.querySelectorAll(".buttons button");
 
 // create the variables to remember the state
 
-let currentInput = "0";
+let currentInput = 0;
 let previousInput = null;
 let operator = null;
 let shouldResetDisplay = false;
 
-// handles numbers buttons
+// handles numbers 
 
 function inputNumber(number) {
   if (currentInput === 0 || shouldResetDisplay) {
@@ -24,27 +24,91 @@ function inputNumber(number) {
   updateDisplay();
 }
 
+// handle decimal number 
+
+function inputDecimal(){
+  if(shouldResetDisplay){
+    currentDisplay = ".";
+    shouldResetDisplay = false;
+  }
+  else if(!currentInput === "."){
+    currentInput +=".";
+  }
+  updateDisplay();
+}
+
 // handlig update number
 function updateDisplay() {
   currentDisplay.textContent = currentInput;
 
-  if (previousDisplay !== null && operator !== null) {
-    previousDisplay.textContent = `${previousDisplay} ${operator}`;
+  if (previousDisplay !== null && operator !== null ) {
+    previousDisplay.textContent = `${previousInput} ${operator}`;
   } else {
     previousDisplay.textContent = "";
   }
 }
 
 // handles operators
-
 function chooseOperator(selectOperator) {
-  previousInput = number(currentInput);
+  previousInput = Number(currentInput);
   operator = selectOperator;
   shouldResetDisplay = true;
 
   updateDisplay();
 }
 
+
+// actual calculation section
+function calculate() {
+  if (previousInput === null || operator === null) {
+    return;
+  }
+
+  const firstNumber = previousInput;
+  const secondNumber = Number(currentInput);
+
+  let result;
+
+  switch (operator) {
+    case "+":
+      result = firstNumber + secondNumber;
+      break;
+
+    case "-":
+      result = firstNumber - secondNumber;
+      break;
+
+    case "*":
+      result = firstNumber * secondNumber;
+      break;
+    case "/":
+      if (secondNumber === 0) {
+        currentInput = "Error";
+        previousInput = null;
+        operator = null;
+        updateDisplay();
+        return;
+      }
+      result = firstNumber / secondNumber;
+      break;
+  }
+
+  currentInput = String(result);
+  previousInput = null;
+  operator = null;
+
+  shouldResetDisplay = true;
+  updateDisplay();
+}
+
+
+function percentage(){
+  if(currentInput === "Error"){
+    return;
+  }
+  currentInput = String(Number(currentInput)/100);
+  updateDisplay();
+}
 // track all the buttons
 
 buttons.forEach((button) => {
@@ -53,10 +117,21 @@ buttons.forEach((button) => {
 
     if (!isNaN(value) || value === ".") {
       inputNumber(value);
-    } else if (button.classList.contains("operator")) {
+    } 
+    else if(value === "."){
+      inputDecimal();
+    }
+    else if(value === "%"){
+      percentage();
+
+    }
+    
+    else if (button.classList.contains("operator")) {
       chooseOperator(value);
     } else if (button.classList.contains("equal")) {
       calculate();
     }
   });
 });
+
+updateDisplay();
